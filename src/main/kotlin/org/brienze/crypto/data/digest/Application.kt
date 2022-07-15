6f55cc638b5fac3f6aa12c83f2d40ac339a986dd
Luis Brienze <lfbrienze@gmail.com>
2022-07-15 14:41:39 -0300

    initial commit
    Signed-off-by: Luis Brienze <lfbrienze@gmail.com>

---
+++ src/main/kotlin/org/brienze/crypto/data/digest/Application.kt
@@ -0,0 +1,8 @@
+package org.brienze.crypto.data.digest
+
+import io.micronaut.runtime.Micronaut.*
+
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ src/main/kotlin/org/brienze/crypto/data/digest/DynamoConfiguration.kt
@@ -0,0 +1,12 @@
+package org.brienze.crypto.data.digest
+
+import io.micronaut.context.annotation.Requires
+import javax.validation.constraints.NotBlank
+import io.micronaut.context.annotation.ConfigurationProperties
+
+@Requires(property = "dynamodb.table-name")
+@ConfigurationProperties("dynamodb")
+public interface DynamoConfiguration {
+    @get:NotBlank
+    val tableName: String
+}
