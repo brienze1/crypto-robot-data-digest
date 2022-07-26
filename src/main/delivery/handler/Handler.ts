549f950daa33f1ce048f8c58851f3829ed4e2589
Luis Brienze <lfbrienze@gmail.com>
2022-07-26 10:19:52 -0300

    [DEV] - changed handler log for troubleshooting
    Signed-off-by: Luis Brienze <lfbrienze@gmail.com>

--- src/main/delivery/handler/Handler.ts
+++ src/main/delivery/handler/Handler.ts
@@ -4,9 +4,9 @@ import { logger } from '@/application/config/LoggerConfig';
 import { AnalysisIndicator } from '@/domain/model/AnalysisIndicator';
 
 export const execute = async (event: SQSEvent, context: Context) => {
-    global.correlationId = context.awsRequestId;
     logger.info('new event received');
     logger.info({ event, context });
+    global.correlationId = context.awsRequestId;
 
     const analysisIndicatorDto = JSON.parse(JSON.parse(event?.Records[0]?.body)?.Message || '{}');
 
